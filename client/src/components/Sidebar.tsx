263e1c27e7ae8f9c1a0466bd118ff08428e51383
Asa DeGroff <asa@3fz.org>
2026-04-21 06:40:31 -0600

    new icon for memory extraction debug button

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -699,8 +699,9 @@ export function Sidebar({
                   title="Memory Extraction Debug"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M12 2a4.5 4.5 0 0 0-4.5 4.5v1a4 4 0 0 0-2 7.4A4 4 0 0 0 8 22a4 4 0 0 0 4-4V6.5A4.5 4.5 0 0 0 12 2Z" />
-                    <path d="M12 2a4.5 4.5 0 0 1 4.5 4.5v1a4 4 0 0 1 2 7.4A4 4 0 0 1 16 22a4 4 0 0 1-4-4" />
+                    <ellipse cx="12" cy="5" rx="9" ry="3"/>
+                    <path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"/>
+                    <path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"/>
                   </svg>
                 </button>
               )}
